697ab9f249c9eda8a5fcaad1c94f4e53d41c6884
satosys <satya9pinu@gmail.com>
2018-05-09 22:05:56 -0400

    Modified the structure

--- Cricinfo/spiders/CrickSprider.py
+++ Cricinfo/spiders/CrickSprider.py
@@ -1,31 +1,31 @@
 # -*- coding: utf-8 -*-
 import scrapy
-#from scrapy.selector import HtmlXPathSelector
-
+import logging
 class CrickspriderSpider(scrapy.Spider):
 
     name = 'CrickSprider'
     
-    allowed_domains = ['www.espncricinfo.com']
+    allowed_domains = ['espncricinfo.com']
     
-    start_url = ['www.espncricinfo.com/india/content/player/country.html?country=6']
+    start_urls = ['http://www.espncricinfo.com/india/content/player/country.html?country=6']
 
+    logging.log(logging.INFO, "Loading requests")
     # location of csv file
     custom_settings = {
         'FEED_URI': 'tmp/cricinfo.csv'
     }
-
-    Commmon_url = "www.espncricinfo.com"
 
     def parse(self, response):
         
         players_link = response.xpath("//div[@class='pnl650M']//td[@class='divider']//a/@href").extract()
         
         players_name = response.xpath("//div[@class='pnl650M']//td[@class='divider']//a/text()").extract()
-        
-        for i in range(0,len(players_link)):
-            
-            player_url = super.Commmon_url + str(players_link[i])
+
+        Commmon_url = "http://www.espncricinfo.com"
+
+        #for i in range(0,len(players_link)):
+        for i in range(0,2):
+            player_url = Commmon_url + str(players_link[i])
                
             yield scrapy.Request(player_url, callback=self.parse_plyr_dtls)
             
@@ -33,13 +33,13 @@ class CrickspriderSpider(scrapy.Spider):
         
         #Extracting the content using xpath selectors.
         
-            item['Full_name_player'] = response.xpath(
+            Full_name_player = response.xpath(
             "//* [@id='ciHomeContentlhs']/div[3]/div[2]/div[1]/p[1]/span/text()").extract()
-            item['Born'] = response.xpath("//*[@id='ciHomeContentlhs']/div[3]/div[2]/div[1]/p[2]/span/text()").extract()
-            item['Major_teams'] = response.xpath("//*[@id='ciHomeContentlhs']/div[3]/div[2]/div[1]/p[3]/span/text()").extract()
+            Born = response.xpath("//*[@id='ciHomeContentlhs']/div[3]/div[2]/div[1]/p[2]/span/text()").extract()
+            Major_teams = response.xpath("//*[@id='ciHomeContentlhs']/div[3]/div[2]/div[1]/p[3]/span/text()").extract()
             item['Playing Role'] = response.xpath("//*[@id='ciHomeContentlhs']/div[3]/div[2]/div[1]/p[4]/span/text()").extract()
             item['Batting style'] = response.xpath("//*[@id='ciHomeContentlhs']/div[3]/div[2]/div[1]/p[5]/span/text()").extract()
             item['Bowling style'] = response.xpath("//*[@id='ciHomeContentlhs']/div[3]/div[2]/div[1]/p[6]/span/text()").extract()
             
-            
+
      
